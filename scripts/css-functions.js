e392961ba113fa3ab3dd212ba760a7b2c6f9ca23
Mthobisi-za <mthobisimtho333@gmail.com>
2022-09-27 08:40:59 +0200

    first commit

---
+++ scripts/css-functions.js
@@ -0,0 +1,16 @@
+document.querySelector('.btn-1').addEventListener("mouseover", ()=>{
+    document.querySelector(".arrow-1").style.display= "contents";
+   
+});
+document.querySelector('.btn-1').addEventListener("mouseout", ()=>{
+    document.querySelector(".arrow-1").style.display= "none"
+});
+
+//second button
+
+document.querySelector('.btn-2').addEventListener("mouseover", ()=>{
+    document.querySelector(".arrow-2").style.display= "contents"
+});
+document.querySelector('.btn-2').addEventListener("mouseout", ()=>{
+    document.querySelector(".arrow-2").style.display= "none"
+});
